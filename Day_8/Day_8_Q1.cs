eb641f69d30c863ab33acf615b0033097056193a
Tushank Shingte <61382216+tushank16@users.noreply.github.com>
2020-10-29 23:39:43 +0530

    Q1 and Q4 added

---
+++ Day_8/Day_8_Q1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Day_8_Assignment
+{
+    class Mymeth
+    {
+        public static int sqr(int n)
+        {
+            return n * n;
+        }
+        public static double sqr(double s)
+        {
+            return s * s;
+        }
+    }
+
+    class Day_8_Q1
+    {
+        //Q1.   Create a static class Mymeth having two static  method
+        //      1. public int sqr(int n) :this method return square of a number
+        //      2. public double sqr(double s): this method return square of a number
+        //      Create a entry point class and call this method.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mymeth.sqr(10.2));
+            Console.WriteLine(Mymeth.sqr(5));
+
+        }
+    }
+
+    
+}
---
+++ Day_8/Day_8_Q4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day_8_Assignment
+{
+    class swapdemo
+    {
+        public static void swap(int x, int y)
+        {
+            Console.WriteLine("in swap Before Swap {0} & {1}", x, y);
+            x += y -= x;
+            Console.WriteLine("in swap After Swap {0} & {1}", x, y);
+        }
+    }
+
+    class Day_8_Q4
+    {
+       
+       static void Main(string[] args)
+       {
+                //Q4. Create a class swapdemo having method
+                //    Public void swap(int x, int y)
+                //    Job of this method is to swap two number.
+                //    Create a entry point class  who’s job is to call this method and print swapped data.
+
+                int a = 10, b = 20;
+                swapdemo.swap(a, b);
+        }
+    }
+}
