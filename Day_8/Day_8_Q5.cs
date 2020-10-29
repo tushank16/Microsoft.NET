ffad4ebebcc3222e068473a4160670b607152c2b
Tushank Shingte <61382216+tushank16@users.noreply.github.com>
2020-10-30 00:07:56 +0530

    Q5 and 6 Added

---
+++ Day_8/Day_8_Q5.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Day_8_Assignment
+{
+    class Animal 
+    {
+        public virtual void speak()
+        {
+            Console.WriteLine("I don’t know how to speak");
+        }
+    }
+
+    class cat : Animal
+    {
+       public override void speak()
+        {
+            Console.WriteLine("mewo mewo");
+        }
+    }
+
+    class dog : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("Bhow Bhow");
+        }
+    }
+
+
+    class donkey : Animal
+    {
+ 
+    }
+
+    class showpoly
+    { 
+        public void call(Animal refer)
+        {
+            refer.speak();
+        }
+    }
+
+    class Day_8_Q5
+    {
+        public static void Main(String[] args)
+        {
+            Animal ref1 = new dog();
+            Animal ref2 = new cat();
+            Animal ref3 = new donkey();
+
+            showpoly show = new showpoly();
+            show.call(ref1);
+            show.call(ref2);
+            show.call(ref3);
+
+        }
+    }
+}
---
+++ Day_8/Day_8_Q6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day_8_Assignment
+{
+    class mymath
+    {
+        public static int sum(params int[] b)
+        {
+            int sum = 0;
+            foreach (int a in b)
+            {
+               sum = sum + a;
+            }
+            return sum;
+        }
+    }
+    class Day_8_Q6
+    {
+         //Q6. Create a class mymath having method sum who’s job is to find average.
+         //    This method should work even if it’s called with different number of arguments eg. sum(4,5) ,sum(4,7,8,9)
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(mymath.sum(4, 5));
+            Console.WriteLine(mymath.sum(4, 7, 8, 9));
+            Console.WriteLine(mymath.sum(4, 5 ,6));
+            Console.WriteLine(mymath.sum(1, 2, 3, 4, 5));
+        }
+    }
+}
