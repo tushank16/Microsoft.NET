0d3eeac74c9c0a2c9cf8f0994f6ee67bfe5f82cd
Tushank Shingte <61382216+tushank16@users.noreply.github.com>
2020-11-01 23:45:15 +0530

    Q1 and Q2 done

---
+++ Day_11/Q1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Day_11_Assignment
+{
+    static class math
+    {
+        //Square
+        public static int square(int n)
+        {
+            return n * n;
+        }
+
+        //Cube
+        public static int cube(int n)
+        {
+            return n * n * n;
+        }
+
+        //factorial 
+        public static int factorial(int n)
+        {
+            int fact =1;
+            for (int i = 1; i < n; i++)
+                fact *= n;
+            return fact;
+        }
+    
+    }
+
+    delegate int mymath(int n);
+
+    class Q1
+    {
+        static void Main(string[] args)
+        {
+            mymath ref1 = new mymath(math.square);
+            Console.WriteLine(ref1(5));
+
+            mymath ref2 = new mymath(math.cube);
+            Console.WriteLine(ref2(5));
+
+            mymath ref3 = new mymath(math.factorial);
+            Console.WriteLine(ref3(2));
+
+        }
+    }
+}
---
+++ Day_11/Q2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Day_11_Assignment
+{
+    //deligate accepting 1 int parameter and returning int
+    delegate int mymath(int n);
+
+
+    //non static class math with instance method
+    class math
+    {
+        //Square
+        public int square(int n)
+        {
+            return n * n;
+        }
+
+        //Cube
+        public int cube(int n)
+        {
+            return n * n * n;
+        }
+
+        //factorial 
+        public int factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 2; i <= n; i++)
+                fact *= i;
+            return fact;
+        }
+
+    }
+      
+    class Q2
+    {
+        static void Main(string[] args)
+        {
+            math obj = new math();
+            mymath ref1;
+
+            ref1 = new mymath(obj.square);
+            Console.WriteLine(ref1(5));
+
+            ref1 = new mymath(obj.cube);
+            Console.WriteLine(ref1(5));
+
+            ref1 = new mymath(obj.factorial);
+            Console.WriteLine(ref1(5));
+
+        }
+    }
+}
