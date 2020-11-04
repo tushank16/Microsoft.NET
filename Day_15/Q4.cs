299b3f4d20eaefc69e77b49795dda28e6bb246d9
Tushank Shingte <61382216+tushank16@users.noreply.github.com>
2020-11-04 23:52:02 +0530

    Q4 done

---
+++ Day_15/Q4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15_Assignment
+{
+    class Q4
+    {
+        //Q4 Accept 5 name from user and store it in an array
+        //store all name starting from “v” in to another array and print
+        //    eg input [vita, vidya, dac, edac,dbda]
+        //        O/P [vita, vidya]
+        static void Main(string[] args)
+        {
+            string[] name = new string[5] { "vita", "vidya", "dac", "edac", "dbda" };
+            List<string> vname = new List<string>();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                if(name[i].StartsWith("v"))
+                {
+                    vname.Add(name[i]);
+                }
+            }
+            foreach (string x in vname)
+                Console.Write(x + " ");
+            Console.WriteLine();
+        }
+    }
+} 
