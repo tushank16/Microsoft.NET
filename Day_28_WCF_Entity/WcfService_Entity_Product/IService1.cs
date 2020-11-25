d0c8717a04a289604437bb5e3956342e65c5c719
Tushank Shingte <61382216+tushank16@users.noreply.github.com>
2020-11-25 11:11:02 +0530

    WcfService_RestAPI_with_ADO.NET_Entity_Product

---
+++ Day_28_WCF_Entity/WcfService_Entity_Product/IService1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService_Entity_Product
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+                                   BodyStyle = WebMessageBodyStyle.Bare,
+                                   UriTemplate = "GetProducts")]
+
+        List<Product> GetProducts();
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+                                   BodyStyle = WebMessageBodyStyle.Bare,
+                                   UriTemplate = "ViewProduct/?id={id}")]
+
+        Product ViewProduct(int id);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
+                                   BodyStyle = WebMessageBodyStyle.Bare,
+                                   UriTemplate = "AddProduct")]
+
+        int AddProduct(Product product);
+    }
+}
